ddc6225366c9d99010d70038c6861f539e03f64b
NEVED123 <neved1234@gmail.com>
2022-07-12 20:30:22 -0400

    added comment

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import  MainScreen  from './screens/MainScreen';
 
+/**
+ * This will include a feature to change screens in the future
+ */
 export default function App() {
   return (
     <View style={{flex:1}}>
