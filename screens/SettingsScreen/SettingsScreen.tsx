ffe15d433934e8ecdcaa0b6646f7ec79af316bc7
NEVED123 <neved1234@gmail.com>
2022-07-25 17:09:54 -0400

    added settings screen to tab navigation

--- screens/SettingsScreen/SettingsScreen.tsx
+++ screens/SettingsScreen/SettingsScreen.tsx
@@ -1,22 +1,15 @@
 import { View, Text, StyleSheet, Pressable, SafeAreaView} from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
-import { styles } from './SettingsElements';
+import { settingStyles, SettingButton } from './SettingsElements'
 
-export default function SettingsScreen(){
-    return(
-            <Text>Settings</Text>
-    )
-
-}
-/*
 export default function Settings(){
     return(
-        <LinearGradient colors={['#333333','#000000']} style={styles.background}>
-            <SafeAreaView style={styles.container}>
-                <Text style={styles.settingTitle}>
+        <LinearGradient colors={['#333333','#000000']} style={settingStyles.background}>
+            <SafeAreaView style={settingStyles.container}>
+                <Text style={settingStyles.settingTitle}>
                     Settings
                 </Text>
-                <View style={styles.settingsBox}>
+                <View style={settingStyles.settingsBox}>
                     <SettingButton text='Light Mode'/>
                     <SettingButton text='Background Mode'/>
                     <SettingButton text='Flashlight on Downbeat'/>
@@ -30,30 +23,5 @@ export default function Settings(){
 
 }
 
-type Props={
-    text:string
-}
-function SettingButton({text= ''}:Props){
-    return(
-        <Pressable style={({ pressed }) => [
-            {
-              backgroundColor: pressed
-                ? '#707070'
-                : '#3f3f3f'
-            },
-            {
-                height:50,
-                borderTopColor:'#909090',
-                borderTopWidth:1,
-                justifyContent:'center'
-                //figure out shadow in buttons, this is apparently a nightmare to do with the "overflow:'hidden'" style in parent container
-            },
-          ]}>
-            <Text style={styles.settingText}>
-                {text}
-            </Text>
-        </Pressable>
-    )
-}
 
-*/
+
