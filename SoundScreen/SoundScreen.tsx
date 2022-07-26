4c9f39067b26ad230bdcd11c6802171c0ab6bc17
Jonathon <Jonathon.esbri@gmail.com>
2022-07-26 14:39:17 -0400

    added more tabs

---
+++ SoundScreen/SoundScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet, Pressable, SafeAreaView} from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
+import { styles } from './SoundElements';
+
+export default function SoundScreen(){
+    return(
+            <Text>Sound</Text>
+    )
+
+}
--- navigation/tabs.tsx
+++ navigation/tabs.tsx
@@ -1,7 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import SettingsScreen from '../screens/SettingsScreen/SettingsScreen';
-
+import TunerScreen from '../screens/TunerScreen/TunerScreen';
+import SoundScreen from '../SoundScreen/SoundScreen';
 const Tab = createBottomTabNavigator();
 
 function Tabs() {
@@ -18,11 +19,12 @@ function Tabs() {
           backgroundColor: '#303030',
           borderRadius: 15,
           height: 90,
-        
         }
       }}
       >
       <Tab.Screen name="Metronome" component={HomeScreen} />
+      <Tab.Screen name="tuner" component={TunerScreen} />
+      <Tab.Screen name='Sounds' component={SoundScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
