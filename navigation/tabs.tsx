0cfa593b0fd9597d748ef5848685f6c9ab44244c
Jonathon <Jonathon.esbri@gmail.com>
2022-07-26 14:13:09 -0400

    tab styling

--- navigation/tabs.tsx
+++ navigation/tabs.tsx
@@ -6,8 +6,23 @@ const Tab = createBottomTabNavigator();
 
 function Tabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Navigator
+    screenOptions = {{
+      headerShown: false,
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 25,
+          left:20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: '#303030',
+          borderRadius: 15,
+          height: 90,
+        
+        }
+      }}
+      >
+      <Tab.Screen name="Metronome" component={HomeScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
