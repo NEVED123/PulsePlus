2ab08184697c3baa510c683ebec4eb631d86f1c0
NEVED123 <neved1234@gmail.com>
2022-07-26 13:58:40 -0400

    gradient background

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,27 @@
-import { StyleSheet, View, Text, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, Button, StatusBar } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 /**
  * The components will be broken down into individual functions once the main screen layout is complete
  */
 export default function HomeScreen() {
     return (
-        <Text>HOME</Text>
+        <LinearGradient colors={['#666666','#333333']} style={homeStyles.container}>
+            <SafeAreaView style={homeStyles.background}>
+                <StatusBar/>
+
+            </SafeAreaView>
+        </LinearGradient>
     );
 }
+
+const homeStyles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    background:{
+        flex:1
+    }
+})
 
 
