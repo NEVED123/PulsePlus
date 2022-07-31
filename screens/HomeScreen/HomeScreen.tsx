141329971c113695e042f788301628cb32314501
NEVED123 <neved1234@gmail.com>
2022-07-31 11:28:11 -0400

    refactored, added metronome block ID's, and added status bar

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,16 +1,18 @@
-import { StyleSheet, View, Text, SafeAreaView, Pressable, StatusBar } from 'react-native';
+import { View, Text, SafeAreaView, Pressable, StatusBar } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useState } from 'react'
-//import { homeStyles } from './HomeElements'
+import { useState } from 'react';
+import { homeStyles } from './HomeElements';
+//import ModalDropdown from 'react-native-modal-dropdown'
 
 /**
  * The components will be broken down into individual functions once the main screen layout is complete
  */
 export default function HomeScreen() {
-    const [meter, setMeter] = useState(new Array(16).fill(0))
+    const [meter, setMeter] = useState(new Array(4).fill(0))
     return (
         <LinearGradient colors={['#666666','#333333']} style={homeStyles.container}>
             <SafeAreaView style={homeStyles.background}>
+                <StatusBar/>
                 <ClickSpace meter={meter}/>
                 <TimeSignature/>
                 <TempoWheel/>
@@ -31,15 +33,14 @@ export function ClickSpace({meter=[0,0,0,0]}:Meter){
             <MetronomeBlockGroup meter={meter}/>
         </View>
     )
-
 }
 
-
-
 export function MetronomeBlockGroup({meter=[]}:Meter){
+    //The 'index' gives each metronome block a seperate ID based on its position in the array, for now its only purpose
+    //is to get a warning to shut up but it will probably become useful
     return(
         <View style={homeStyles.metronomeBlockGroup}>
-            {meter.map(x => <MetronomeBlock/>)}
+            {meter.map((x,index) => <MetronomeBlock key={index}/>)}
         </View>
     )
 }
@@ -114,120 +115,5 @@ export function StartButton(){
         </Pressable>
     )
 }
-
-const homeStyles = StyleSheet.create({
-    container:{
-        flex:1
-    },
-    background:{
-        flex:1,
-    },
-    clickSpace:{
-        flex:1
-    },
-    metronomeBlock:{
-        flex:1,
-        backgroundColor:'#D9D9D9',
-        margin:5,
-        shadowColor: "#000",
-        shadowOffset: {
-	        width: 0,
-	        height: 4,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5
-    },
-    metronomeBlockGroup:{
-        flex:1,
-        flexDirection:'row',
-        padding:20
-    },
-    timeSignature:{
-        height:75,
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    timeSignatureNumberSelector:{
-        backgroundColor:'#D9D9D9',
-        shadowColor: "#000",
-        shadowOffset: {
-	        width: 0,
-	        height: 4,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5
-    },
-    timeSignatureNumber:{
-        fontSize:30,
-        lineHeight:75,
-        width:75, 
-        textAlign:'center'
-    },
-    timeSignatureDivider:{
-        fontSize:30, 
-        color:'#D9D9D9', 
-        marginLeft:20, 
-        marginRight:20,
-        textShadowColor: "#000",
-        textShadowOffset: {
-            width: 0, 
-            height: 4
-        },
-        textShadowRadius: 4
-    },
-    tempoWheel:{
-        backgroundColor:'#D9D9D9',
-        width: 250,
-        height: 250,
-        borderRadius: 125,
-        marginTop:20,
-        marginLeft:20,
-        marginRight:20,
-        marginBottom:5,
-        shadowColor: "#000",
-        shadowOffset: {
-	        width: 0,
-	        height: 4,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5
-    },
-    tempoText:{
-        color:'white',
-        fontSize:36,
-        textShadowColor: "#000",
-        textShadowOffset: {
-            width: 0, 
-            height: 4
-        },
-        textShadowRadius: 4,
-        marginBottom:20
-    },
-    tempoDirectionText:{
-        //text for + and - for wheel
-        fontSize:36,
-        color:'white'
-    },
-    startButton:{
-        height:75,
-        borderColor:'#909090',
-        borderWidth:1,
-        borderRadius:20,
-        alignItems:'center',
-        justifyContent:'center',
-        marginLeft:20,
-        marginRight:20,
-        marginBottom:20,
-        //figure out shadow in buttons, this is apparently a nightmare to do with the "overflow:'hidden'" style in parent container
-    },
-    startText:{
-        color:'white',
-        fontSize:36
-    }
-})
 
 
