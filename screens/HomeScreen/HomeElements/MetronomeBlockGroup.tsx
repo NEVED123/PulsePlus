19f56fab4885e5db6d44f580950ab63fb5cde2a5
NEVED123 <neved1234@gmail.com>
2022-08-04 21:10:40 -0400

    added second row for numerator over 16

--- screens/HomeScreen/HomeElements/MetronomeBlockGroup.tsx
+++ screens/HomeScreen/HomeElements/MetronomeBlockGroup.tsx
@@ -4,15 +4,44 @@ import { MetronomeBlock } from './MetronomeBlock'
 export function MetronomeBlockGroup({ meter, setMeter }:{ meter:number[], setMeter:Function}){
     //The 'index' gives each metronome block a seperate ID based on its position in the array, for now its only purpose
     //is to get a warning to shut up but it will probably become useful
-    return(
-        <View style={styles.metronomeBlockGroup}>
-            {meter.map((x,index) => <MetronomeBlock 
+    if(meter.length <= 16){
+        return(      
+            <View style={styles.metronomeBlockGroup}>
+                {meter.map((x,index) => <MetronomeBlock 
+                    key={index} 
+                    beatNumber={index} 
+                    meter={meter} 
+                    setMeter={setMeter}/>)}
+            </View>
+        )
+    }
+    else{ //CURRENTLY DISAPEARS
+        const topRow = meter.slice(0,15)
+        const bottomRow = meter.slice(16, meter.length)
+        console.log(topRow)
+        console.log(bottomRow)
+        return(
+            <View style={{flex:1}}>
+                <View style={styles.metronomeBlockGroup}>
+                {topRow.map((x,index) => <MetronomeBlock 
                 key={index} 
                 beatNumber={index} 
                 meter={meter} 
                 setMeter={setMeter}/>)}
-        </View>
-    )
+                </View>
+                <View style={styles.metronomeBlockGroup}>
+                {bottomRow.map((x,index) => <MetronomeBlock 
+                key={index} 
+                beatNumber={index} 
+                meter={meter} 
+                setMeter={setMeter}/>)}
+                </View> 
+            </View>
+
+        )
+
+    }
+
 }
 
 const styles = StyleSheet.create({
