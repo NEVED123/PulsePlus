b5d05f0229e6242d41c8f4b1c74e2b1f109e37dd
NEVED123 <neved1234@gmail.com>
2022-08-20 13:56:40 -0400

    added tab bar icons

--- navigation/tabs.tsx
+++ navigation/tabs.tsx
@@ -3,9 +3,11 @@ import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import SettingsScreen from '../screens/SettingsScreen/SettingsScreen';
 import TunerScreen from '../screens/TunerScreen/TunerScreen';
 import SoundScreen from '../SoundScreen/SoundScreen';
+import { View, Image, StyleSheet } from 'react-native'
+
 const Tab = createBottomTabNavigator();
 
-function Tabs() {
+export default function Tabs() {
   return (
     <Tab.Navigator
     screenOptions = {{
@@ -23,12 +25,64 @@ function Tabs() {
         }
       }}
       >
-      <Tab.Screen name="Metronome" component={HomeScreen} />
-      <Tab.Screen name="tuner" component={TunerScreen} />
-      <Tab.Screen name='Sounds' component={SoundScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Metronome" component={HomeScreen} 
+        options={{tabBarIcon: ()=>{
+          return(
+            <View style={styles.iconContainer}>
+              <Image
+              source={require('../assets/tab-logos/MetronomeLogo.png')}
+              style={styles.image}
+              />
+            </View>
+          )
+        }}}/>
+      <Tab.Screen name="Tuner" component={TunerScreen}
+        options={{tabBarIcon: ()=>{
+          return(
+            <View style={styles.iconContainer}>
+              <Image
+              source={require('../assets/tab-logos/TuningForkLogo.png')}
+              style={styles.image}
+              />
+            </View>
+          )
+        }}}/>      
+      <Tab.Screen name='Sounds' component={SoundScreen}
+        options={{tabBarIcon: ()=>{
+          return(
+            <View style={styles.iconContainer}>
+              <Image
+              source={require('../assets/tab-logos/SoundsLogo.png')}
+              style={{
+                height:40,
+                width:40
+              }}
+              />
+            </View>
+          )
+        }}}/>  
+      <Tab.Screen name="Settings" component={SettingsScreen}
+        options={{tabBarIcon: ()=>{
+          return(
+            <View style={styles.iconContainer}>
+              <Image
+              source={require('../assets/tab-logos/SettingsLogo.png')}
+              style={styles.image}
+              />
+            </View>
+          )
+        }}}/>  
     </Tab.Navigator>
   );
 }
 
-export default Tabs;
+const styles = StyleSheet.create({
+  image:{
+    height:40,
+    width:30
+  },
+  iconContainer:{
+    alignItems:'center', 
+    justifyContent:'center'
+  }
+})
