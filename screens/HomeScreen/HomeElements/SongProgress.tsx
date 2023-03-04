79634a7c8d9466af81d73f9027a70744340eec12
NEVED123 <neved1234@gmail.com>
2023-03-04 09:11:58 -0500

    added section name and counter to progress indicator

--- screens/HomeScreen/HomeElements/SongProgress.tsx
+++ screens/HomeScreen/HomeElements/SongProgress.tsx
@@ -1,16 +1,29 @@
 import { Slider } from "@miblanchard/react-native-slider";
 import { useState, useEffect, useContext } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { PreferencesContext } from "../../../logic/PreferencesManager";
 import { SongContext } from "../../../logic/SongManager";
+import { textColors } from "../../../theme/Colors";
 
 export function SongProgress(){
 
-    const { activeMeterIndex, setActiveMeterIndex, length, running } = useContext(SongContext)
+    const { activeMeterIndex, setActiveMeterIndex, length, running, sectionName } = useContext(SongContext)
     const [sliderValue, setSliderValue] = useState(activeMeterIndex)
+    const { theme } = useContext(PreferencesContext)
+
+    useEffect(()=>{
+        setSliderValue(activeMeterIndex)
+    }, [activeMeterIndex])
 
     return(
         <View>
-            {(length > 1) && <Slider
+            {(length > 1) && <View>
+                <Text style={[
+                    {color: textColors[theme as keyof typeof textColors]},
+                    styles.text]}>
+                    {`${sectionName != undefined ? sectionName : `Section ${activeMeterIndex+1}`} - ${activeMeterIndex+1}/${length}`}
+                </Text>
+                <Slider
                 containerStyle={styles.container}
                 value={sliderValue}
                 onValueChange={(value)=>{
@@ -23,7 +36,8 @@ export function SongProgress(){
                 step={1}
                 disabled={running}
                 trackClickable={true}
-            />}
+                />
+            </View>}
         </View>
     )
 }
@@ -31,7 +45,12 @@ export function SongProgress(){
 const styles = StyleSheet.create({
    container:{
     marginHorizontal:20,
-    marginVertical:10
+    marginBottom:10
+   },
+   text:{
+    alignSelf:'center',
+    fontSize:20,
+    marginTop:10
    }
 
 })
