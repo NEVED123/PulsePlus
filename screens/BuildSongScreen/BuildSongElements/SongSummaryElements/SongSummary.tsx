7f9b6c73a2ab8bb9e440b8758d0677a91c4df2a5
NEVED123 <neved1234@gmail.com>
2023-03-13 20:39:35 -0400

    added button panel to song summary section

--- screens/BuildSongScreen/BuildSongElements/SongSummaryElements/SongSummary.tsx
+++ screens/BuildSongScreen/BuildSongElements/SongSummaryElements/SongSummary.tsx
@@ -1,13 +1,15 @@
-import { Text, SafeAreaView, StyleSheet, ScrollView} from 'react-native'
+import { Text, SafeAreaView, StyleSheet, ScrollView, View} from 'react-native'
 import { backgroundColors } from '../../../../theme/Colors'
 import { textTitleColors } from '../../../../theme/Colors'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 import { PreferencesContext } from '../../../../logic/PreferencesManager'
 import { BuildSongButton } from '../BuildSongButton'
 import SongSummaryItem from './SongSummaryItem'
 import { Meter, Beat, Song } from '../../../../logic/structure'
 import { BuildSongContext } from '../../../../logic/BuildSongManager'
+import SaveSongDialong from '../SaveSongDialog'
+import { Portal } from 'react-native-paper'
 
 export default function SongSummary({navigation} : {navigation : any}){
 
@@ -15,22 +17,32 @@ export default function SongSummary({navigation} : {navigation : any}){
 
     const { song } : { song: Song }= useContext(BuildSongContext)
 
+    const [dialogVisible, setDialogVisible] = useState(false)
+
     const summaryItemArray = song.song.map((meter, index)=>{
         return <SongSummaryItem meter={meter} index={index}/>
     })
 
     return(
-        <LinearGradient colors={backgroundColors[theme as keyof typeof backgroundColors]} style={styles.background}>
-            <SafeAreaView style={styles.container}>
-                    <BuildSongButton text={'< Back'} onPress={()=>{navigation.navigate('BuildSongMenu')}}/>
+        <Portal.Host>
+            <LinearGradient colors={backgroundColors[theme as keyof typeof backgroundColors]} style={styles.background}>
+                <SaveSongDialong visible={dialogVisible} setVisible={setDialogVisible}/>
+                <SafeAreaView style={styles.container}>
+                    <View style={styles.buildSongButtonPanel}>
+                        <BuildSongButton 
+                            text={'< Back'} onPress={()=>{navigation.navigate('BuildSongMenu')}}/>
+                        <BuildSongButton text={'Save'} onPress={()=>{setDialogVisible(!dialogVisible)}}/>
+                        <BuildSongButton text={'Load'} onPress={()=>{}}/>
+                    </View>
                     <Text style={[styles.title, {color : textTitleColors[theme as keyof typeof textTitleColors]}]}>
                         Summary
                     </Text>
                     <ScrollView>
                         {summaryItemArray}
                     </ScrollView>
-            </SafeAreaView>        
-        </LinearGradient> 
+                </SafeAreaView>        
+            </LinearGradient> 
+        </Portal.Host>
     )
 }
 
@@ -53,5 +65,11 @@ const styles = StyleSheet.create({
         margin:20,
         borderBottomColor:'#909090', //the settings all have top border only
         borderBottomWidth:1
+    },
+    buildSongButtonPanel:{
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        marginVertical:10
     }
 })
